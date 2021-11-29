22154f90b6d0d77beff1ec2650c0b5a29ca37047
Robbie Antenesse <dev@alamantus.com>
2021-11-29 14:30:29 -0700

    Change default build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,7 @@
 var gulp = require('gulp');
 
-gulp.task('default', fromScss);
-
-function fromScss(done) {
-    var sass = require('gulp-sass')(require('node-sass'));
-    var cleanCSS = require('gulp-clean-css');
-    var cssnano = require('cssnano');
-    var csso = require('postcss-csso');
-    var postcss = require('gulp-postcss');
-    var size = require('gulp-size');
-    var gzip = require('gulp-gzip');
-    return gulp.src('src/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(cleanCSS({ level: 2 }))
-        .pipe(postcss([cssnano({ preset: 'advanced' }), csso]))
-        .pipe(size({ showFiles: true }))
-        .pipe(gulp.dest('dist/'))
-        .pipe(gzip())
-        .pipe(size({ showFiles: true }))
-        .pipe(gulp.dest('dist/'));
-    done();
-}
-gulp.task('from-css', fromCss);
+gulp.task('default', fromCss);
+gulp.task('from-scss', fromScss);
 
 function fromCss(done) {
     var postcss = require('gulp-postcss');
@@ -43,8 +23,28 @@ function fromCss(done) {
         .pipe(gulp.dest('dist/'));
     done();
 }
+function fromScss(done) {
+    var sass = require('gulp-sass')(require('node-sass'));
+    var cleanCSS = require('gulp-clean-css');
+    var cssnano = require('cssnano');
+    var csso = require('postcss-csso');
+    var postcss = require('gulp-postcss');
+    var size = require('gulp-size');
+    var gzip = require('gulp-gzip');
+    return gulp.src('src/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS({ level: 2 }))
+        .pipe(postcss([cssnano({ preset: 'advanced' }), csso]))
+        .pipe(size({ showFiles: true }))
+        .pipe(gulp.dest('dist/'))
+        .pipe(gzip())
+        .pipe(size({ showFiles: true }))
+        .pipe(gulp.dest('dist/'));
+    done();
+}
+
 gulp.task('watch', function(){
-    gulp.series(['default', 'from-css'])
-    gulp.watch('src/**/*.scss').on('change', gulp.series('default'));
-    gulp.watch('src/*.css').on('change', gulp.series('from-css'));
+    gulp.series(['default', 'from-scss']);
+    gulp.watch('src/**/*.scss').on('change', gulp.series('from-scss'));
+    gulp.watch('src/*.css').on('change', gulp.series('default'));
 })

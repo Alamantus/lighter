8b6cd3089c55bd6a04d2acf24cb3e8dfd7895896
Robbie Antenesse <dev@alamantus.com>
2021-11-29 13:06:06 -0700

    Ensure all gulp deps are in devDependencies; upgrade all deps

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,8 @@ var gulp = require('gulp');
 gulp.task('default', css);
 
 function css(done) {
-    var sass = require('gulp-sass');
-    sass.compiler = require('node-sass');
+    var sass = require('gulp-sass')(require('node-sass'));
+    var cleanCSS = require('gulp-clean-css');
     var cssnano = require('cssnano');
     var csso = require('postcss-csso');
     var postcss = require('gulp-postcss');
@@ -12,7 +12,8 @@ function css(done) {
     var gzip = require('gulp-gzip');
     return gulp.src('src/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(postcss([cssnano({ preset: "advanced" }), csso]))
+        .pipe(cleanCSS({ level: 2 }))
+        .pipe(postcss([cssnano({ preset: 'advanced' }), csso]))
         .pipe(size({ showFiles: true }))
         .pipe(gulp.dest('dist/'))
         .pipe(gzip())
@@ -25,14 +26,21 @@ gulp.task('minify', minify);
 function minify(done) {
     var postcss = require('gulp-postcss');
     var cleanCSS = require('gulp-clean-css');
+    var cssnano = require('cssnano');
+    var csso = require('postcss-csso');
+    var postcss = require('gulp-postcss');
     var cssvariables = require('postcss-css-variables');
+    var size = require('gulp-size');
     return gulp.src('src/*.css')
         .pipe(postcss([cssvariables()]))
+        .pipe(postcss([cssnano({ preset: 'advanced' }), csso]))
         .pipe(cleanCSS({ level: 2 }))
+        .pipe(size({ showFiles: true }))
         .pipe(gulp.dest('dist/'))
     done();
 }
 gulp.task('watch', function(){
-    gulp.series('minify')
-    gulp.watch("src/*.css").on('change', gulp.series('minify'));
+    gulp.series(['default', 'minify'])
+    gulp.watch('src/**/*.scss').on('change', gulp.series('default'));
+    gulp.watch('src/*.css').on('change', gulp.series('minify'));
 })
